b1e03bff26b2743d3f24855fa4ede9c87ca29976
SimonBesnard <simon.besnard@wur.nl>
2015-06-18 18:44:12 +0200

    Progress on flux net data analysis

--- Scripts/FluxNet_Process.R
+++ Scripts/FluxNet_Process.R
@@ -13,6 +13,7 @@
 # install.packages("dplyr")
 # install.packages("testthat")
 # install.packages('gtools')
+install.packages("FactoMineR")
 
 library(RNetCDF)
 library (ggplot2)
@@ -24,6 +25,7 @@ library (dplyr)
 library (plyr)
 require(testthat)
 library(gtools)
+library(FactoMineR)
 
 #1. Create a df for all fluxnet sites dataframe
 
@@ -43,51 +45,16 @@ Fluxnet_Site<-Fluxnet_Site[sapply(Fluxnet_Site, function(x) !inherits(x, "try-er
 
 # Remove data before disturbance from the list of dataframe
 Site_Date<-read.csv("Input/Potential_Sites.csv", header = TRUE)
-# Fluxnet_Site[1][!(Fluxnet_Site$DateTime %in% Site_Date$Measure.Date[1]),]
-# 
-# Fluxnet_Site[1]<-with(Fluxnet_Site[1], Fluxnet_Site[1][(Fluxnet_Site[1]$DateTime <= Site_Date$Measure.Date),])
-# subset(Fluxnet_Site[1], Fluxnet_Site[1]$DateTime >= Site_Date$Measure.Date[1])
+Site_Date$Measure_Date<- as.Date(Site_Date$Measure_Date)
+for (i in seq_along(Fluxnet_Site)){
+  Fluxnet_Site[[i]]$DateTime=as.Date(Fluxnet_Site[[i]]$DateTime)
+}
 
-#2. Analyse annual variability of the fluxes
+for (i in seq_along(Fluxnet_Site)){
+Fluxnet_Site[[i]]<-with(Fluxnet_Site[[i]], Fluxnet_Site[[i]][(Fluxnet_Site[[i]]$DateTime > Site_Date$Measure_Date[i]),])
+}
 
-#Merge all site together
-dfAll_Sites_Raw<- do.call("rbind", Fluxnet_Site)
-
-#Plot data raw flux data
-# NEE
-p1<-ggplot(dfAll_Sites_Raw, aes(dfAll_Sites_Raw$DateTime, dfAll_Sites_Raw$NEE_f[order(dfAll_Sites_Raw$DateTime)])) +
-  geom_line() +
-  # geom_path()+
-  # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
-  xlab("Year since disturbance") + ylab("NEE (g.m-2.day-1)")+ 
-  theme_bw(base_size = 12, base_family = "Helvetica") + 
-  theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-#GPP
-p2<-ggplot(dfAll_Sites_Raw, aes(dfAll_Sites_Raw$DateTime, dfAll_Sites_Raw$GPP_f[order(dfAll_Sites_Raw$DateTime)])) +
-  geom_line() +
-  # geom_path()+
-  # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
-  xlab("Year since disturbance") + ylab("NEE (g.m-2.day-1)")+ 
-  theme_bw(base_size = 12, base_family = "Helvetica") + 
-  theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-#TER
-p3<-ggplot(dfAll_Sites_Raw, aes(dfAll_Sites_Raw$DateTime, dfAll_Sites_Raw$Reco[order(dfAll_Sites_Raw$DateTime)])) +
-  geom_line() +
-  # geom_path()+
-  # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
-  xlab("Year since disturbance") + ylab("NEE (g.m-2.day-1)")+ 
-  theme_bw(base_size = 12, base_family = "Helvetica") + 
-  theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggsave(p3, filename = 'Latex/Figures/BR_Sa3/BR_Sa3_TER.png', width = 12, height = 4)
-
-#3. Analyse inter-annual variability of the fluxe
+#3. Analyse inter-annual variability of the fluxex
 
 # Compute mean annual +/- sd
 Mean_Sd_Flux<-lapply(Fluxnet_Site, function (x) ddply(x, "year",
@@ -99,52 +66,93 @@ Mean_Sd_Flux<-lapply(Fluxnet_Site, function (x) ddply(x, "year",
                     sd_GPP= sd(GPP_f, na.rm=T),
                     sd_Reco= sd(Reco, na.rm=T)))
 
-#Remove data before disturbance for each site and Year after disturbance
-Mean_Sd_Flux[[28]]<-Mean_Sd_Flux[[28]][-c(1:3),]
-Mean_Sd_Flux[[29]]$Year_Disturbance<- c("0","1", "2", "3", "4", "5", "6")
-saveRDS(Mean_Sd_Flux, file="Output/Mean_Sd_Flux.rds")
+#Add type of ecosystem, type of climate, type of disturbance and year after disturbance for each sites
+for (i in seq_along(Mean_Sd_Flux)){
+  Mean_Sd_Flux[[i]]$Ecosytem<- Site_Date$Ecosystem[i]
+  Mean_Sd_Flux[[i]]$Climate<- Site_Date$Climate[i]
+  Mean_Sd_Flux[[i]]$Disturbance<- Site_Date$Type_Disturbance[i]
+  Mean_Sd_Flux[[i]]$Year_Disturbance<- Mean_Sd_Flux[[i]]$year- year(Site_Date$Measure_Date[i])
+}
 
 #Combine the flux sites in one dataframe
-source('Function/dfColRename.R')
 dfAll_Sites<- do.call("rbind", Mean_Sd_Flux)
-dfAll_Sites$Year_Disturbance <- as.integer(as.character(dfAll_Sites$Year_Disturbance))
-dfAll_Sites<- dfAll_Sites[-c(76),] #Remove outlier
-saveRDS(dfAll_Sites, file="Output/dfAll_Sites.rds")
+dfAll_Sites<-dfAll_Sites[-c(53),]# the measurements seems to be an outlier
 
 #Plot data mean/sd flux data
-
-#NEE
 limits_NEE <- aes(ymax = mean_NEE + sd_NEE, ymin=mean_NEE - sd_NEE)
 limits_GPP <- aes(ymax = mean_GPP + sd_GPP, ymin=mean_GPP - sd_GPP)
 limits_TER <- aes(ymax = mean_TER + sd_Reco, ymin=mean_TER - sd_Reco)
 
-gg1<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_NEE[order(dfAll_Sites$Year_Disturbance)])) +
+#NEE
+gg1<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_NEE)) +
+  facet_wrap(~Disturbance, ncol=1)+
   geom_point(size=2, shape=3) +
   # geom_path()+
   # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
-  xlab("Year since disturbance") + ylab("Annual Mean-NEE (g.m-2.day-1)")+ 
+  xlab("Year since Disturbance") + ylab("Annual Mean-NEE (g.m-2.day-1)")+ 
   theme_bw(base_size = 12, base_family = "Helvetica") + 
   theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-gg2<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_GPP[order(dfAll_Sites$Year_Disturbance)])) +
+#GPP
+gg2<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_GPP)) +
+  facet_wrap(~Disturbance, ncol=1)+
   geom_point(size=2, shape=3) +
   # geom_path()+
-  # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
+  # geom_errorbar(limits_GPP, width=0.07, linetype=6)+
   xlab("Year since disturbance") + ylab("Annual Mean-GPP (g.m-2.day-1)")+ 
   theme_bw(base_size = 12, base_family = "Helvetica") + 
   theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-gg3<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_TER[order(dfAll_Sites$Year_Disturbance)])) +
+#TER
+gg3<-ggplot(dfAll_Sites, aes(dfAll_Sites$Year_Disturbance, dfAll_Sites$mean_TER)) +
   geom_point(size=2, shape=3) +
+  facet_wrap(~Disturbance, ncol=1)+
   # geom_path()+
-  # geom_errorbar(limits_NEE, width=0.07, linetype=6)+
+  # geom_errorbar(limits_TER, width=0.07, linetype=6)+
   xlab("Year since disturbance") + ylab("Annual Mean-TER (g.m-2.day-1)")+ 
   theme_bw(base_size = 12, base_family = "Helvetica") + 
   theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggsave(gg3, filename = 'Latex/Figures/All_Sites_TER_Mean.png', width = 10, height = 6)
+ggsave(gg3, filename = 'Latex/Figures/Dist_TER_Mean.eps', width = 10, height = 8)
+
+#4 Explain variabilty of the fluxes
+
+#4.1. Fluxes
+
+#NEE
+fit = lm(mean_NEE ~ Climate + Ecosytem + Disturbance, data=dfAll_Sites)
+summary(fit)
+af<-anova(fit)
+afss <- af$"Sum Sq"
+print(cbind(af,PctExp=afss/sum(afss)*100))
+
+m3 = fit
+m2 = update(m3, ~ . - Ecosytem)
+m1 = update(m2, ~ . - Climate)
+af<-anova(m1,m2,m3)
+afss <- af$"Sum of Sq"
+print(cbind(af,PctExp=afss/sum(afss)*100))
 
 
+#GPP
+fit = lm(mean_GPP ~ Climate + Ecosytem + Disturbance, data=dfAll_Sites)
+summary(fit)
+
+m3 = fit
+m2 = update(m3, ~ . - Disturbance)
+m1 = update(m2, ~ . - Ecosytem)
+
+anova(m1,m2,m3)
+
+#TER
+fit = lm(mean_TER ~ Climate + Ecosytem + Disturbance, data=dfAll_Sites)
+summary(fit)
+
+m3 = fit
+m2 = update(m3, ~ . - Disturbance)
+m1 = update(m2, ~ . - Ecosytem)
+
+anova(m1,m2,m3)
