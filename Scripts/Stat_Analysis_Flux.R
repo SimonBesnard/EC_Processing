3e2f4bb4d675e259f4baa19e9c4a497a16391842
SimonBesnard <simon.besnard@wur.nl>
2015-09-20 18:27:47 +0200

    Random forest 1.3

--- Scripts/Stat_Analysis_Flux.R
+++ Scripts/Stat_Analysis_Flux.R
@@ -25,107 +25,29 @@ library (reshape)
 # Import dataframe
 dfAll_Sites<-readRDS("Output/df_Annual_Flux.rds")
 
-#1.1. Anova analysis
-
-#Subset dataset
+#1.1. Subset dataset
 Flux_High<-dfAll_Sites[dfAll_Sites$Int_Replacement %in% c("High"),]
 GPP_High<-Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
 Reco_High<-Flux_High[Flux_High$Type_Flux %in% c("Respiration"),]
-NEE_High<-Flux_High[Flux_High$Type_Flux %in% c("NEE"),]
+Ratio_High<-Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
+NEP_High<-Flux_High[Flux_High$Type_Flux %in% c("NEP"),]
+
+# Harvest
 GPP_High_Harvest<-GPP_High[GPP_High$Disturbance %in% c("Harvest"),]
 Reco_High_Harvest<-Reco_High[Reco_High$Disturbance %in% c("Harvest"),]
+NEP_High_Harvest<-NEP_High[NEP_High$Disturbance %in% c("Harvest"),]
+Ratio_High_Harvest<-Ratio_High[Ratio_High$Disturbance %in% c("Harvest"),]
+
+#Fire
 GPP_High_Fire<-GPP_High[GPP_High$Disturbance %in% c("Wildfire"),]
 Reco_High_Fire<-Reco_High[Reco_High$Disturbance %in% c("Wildfire"),]
-Ratio_High<-Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
-Ratio_High_Harvest<-Ratio_High[Ratio_High$Disturbance %in% c("Harvest"),]
 Ratio_High_Fire<-Ratio_High[Ratio_High$Disturbance %in% c("Wildfire"),]
+NEP_High_Fire<-NEP_High[NEP_High$Disturbance %in% c("Wildfire"),]
 
-#Compute linear model
-fit = lm(values ~ Annual_Preci + Stand_Age +Tair + Tsoil + Rg, data=GPP_High)
-
-m5 = fit
-m4 = update(m5, ~ . - Tair)
-m3 = update(m4, ~ . - Tsoil)
-m2=  update(m3, ~ . - Rg)
-m1=  update(m2, ~ . - Stand_Age)
-m0=  update(m1, ~ . - Annual_Preci)
-
-af<-anova(m0,m1,m2,m3,m4,m5)
-summary(af)
-afss <- af$"Sum of Sq"
-print(cbind(af,PctExp=afss/sum(afss)*100))
-
-#Compute performance of the model
-af<-step(fit, direction = c("forward"), steps = 2000)
-summary(af)
-
-# 1.2 Principal Components Analysis
+# 1. 2. Compute Random Forest on the annual flux
 
 #Subset the data
-Reco_High_PCA<-data.frame(cbind(Reco_High$values, Reco_High$Annual_Preci, Reco_High$Stand_Age,
-                                Reco_High$Tair, Reco_High$Tsoil,
-                                Reco_High$Rg))
-
-colnames(Reco_High_PCA)<-c("Reco", "Precipitation", "Stand_Age", "Tair", "Tsoil", "Rg")
-
-#Plot the pairwise scatterplots
-gg7<-ggpairs(Reco_High_PCA,
-             columns = c(1,2,3,4,5,6),
-             legends=T,
-             lower = list(continuous = "points"),
-             diag = list(continuous = "density"),
-             axisLabels = "none",
-             title = "Correlation matrix - Reco")+ 
-  theme_bw(base_size = 14, base_family = "Helvetica")
-print(gg7)
-
-# Perform the principal component analysis
-arc.pca1<-prcomp(na.omit(Reco_High_PCA), scores=T, cor=T)
-summary(arc.pca1, loadings=T)
-
-# create data frame with scores
-scores = as.data.frame(arc.pca1$x)
-
-# plot of observations
-gg8<-ggplot(data = scores, aes(x = PC1, y = PC2, label = rownames(scores))) +
-  geom_hline(yintercept = 0, colour = "gray65") +
-  geom_vline(xintercept = 0, colour = "gray65") +
-  geom_text(colour = "tomato", alpha = 0.8, size = 4)
-print(gg8)
-
-# function to create a circle of correlations
-circle <- function(center = c(0, 0), npoints = 100) {
-  r = 1
-  tt = seq(0, 2 * pi, length = npoints)
-  xx = center[1] + r * cos(tt)
-  yy = center[1] + r * sin(tt)
-  return(data.frame(x = xx, y = yy))
-}
-corcir = circle(c(0, 0), npoints = 100)
-
-# create data frame with correlations between variables and PCs
-correlations = as.data.frame(cor(na.omit(Reco_High_PCA), arc.pca1$x))
-
-# data frame with arrows coordinates
-arrows = data.frame(x1 = c(0, 0, 0, 0, 0, 0), y1 = c(0, 0, 0, 0, 0, 0), x2 = correlations$PC1, 
-                    y2 = correlations$PC2)
-
-# geom_path will do open circles
-gg9<-ggplot() + geom_path(data = corcir, aes(x = x, y = y), colour = "gray65") + 
-  geom_segment(data = arrows, aes(x = x1, y = y1, xend = x2, yend = y2), colour = "red") + 
-  geom_text(data = correlations, aes(x = PC1, y = PC2, label = rownames(correlations))) + 
-  geom_hline(yintercept = 0, colour = "gray65") + geom_vline(xintercept = 0, colour = "gray65")+
-  xlim(-1.1, 1.1) + ylim(-1.1, 1.1) + labs(x = "pc1 aixs", y = "pc2 axis")+
-  theme_bw(base_size = 12, base_family = "Helvetica") + 
-  theme(panel.grid.minor = element_line(colour="grey", size=0.5)) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  ggtitle("PCA-Reco")
-print(gg9)
-
-# 2. Compute Random Forest on the annual flux
-
-#Subset the data
-GPP_High<-data.frame(cbind(GPP_High$values, GPP_High$Annual_Preci, GPP_High$Stand_Age,
+GPP_High<-data.frame(cbind(GPP_High$Site_ID, GPP_High$values, GPP_High$Annual_Preci, GPP_High$Stand_Age,
                            GPP_High$Tair, GPP_High$Tsoil,
                            GPP_High$Rg))
 
@@ -147,21 +69,13 @@ ggplot(dta, aes(x=GPP, y=value))+
 ## Create a basic Random Forest
 
 # Create Training & Test Sets
-set.seed(42)
 GPP_High<-na.omit(GPP_High)
 rownames(GPP_High)<-1:nrow(GPP_High)
 rows <- sample(x=1:nrow(GPP_High),size=0.7 *  nrow(GPP_High))
 train <- GPP_High[rows,]
 test <-GPP_High[!rownames(GPP_High) %in% rows,]
 
-#Train a Random Foret 
-set.seed(42)
-rf_GPP<-randomForest(GPP~., data=train, ntree = 1000)
-
-# Compute correlation
-cor_rf_GPP<-(cor(predict(rf_GPP,train), train$GPP, use="pairwise"))^2
-
-## Create a final Random forest
+## Create a Random forest with leave one out ID CV
 nobs <- nrow(na.omit(train))
 crossclass <- sample(5,nobs,TRUE)
 crossPredict <- rep(NA,nobs)
