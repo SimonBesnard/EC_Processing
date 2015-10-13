aa629baf4443e0a9015c241a6db1989610e05e4f
SimonBesnard <simon.besnard@wur.nl>
2015-10-13 19:48:08 +0200

    Adapt model to data

--- Scripts/Model_NEP.R
+++ Scripts/Model_NEP.R
@@ -1,5 +1,40 @@
-nyears=100
-GPP <- rep(1000, nyears)
+## Script to model NEP vs. stand age
+## Author: Simon Besnard
+## 12.10.2015
+###################################
+## Load the necessary packages
+
+
+# Import dataframe
+dfAll_Sites<-readRDS("Output/df_Annual_Flux.rds")
+
+#1. Subset dataset
+Flux_High<-dfAll_Sites[dfAll_Sites$Study %in% c("Yes"),]
+
+#Subset type of flux
+
+#NEP
+NEP<-Flux_High[Flux_High$Type_Flux %in% c("NEP"),]
+
+#GPP
+GPP<-Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
+
+#Reco
+Reco<-Flux_High[Flux_High$Type_Flux %in% c("Respiration"),]
+
+#Ratio GPP-Reco
+Ratio_GPP_ER<-Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
+
+#Ratio NEP-GPP
+Ratio_NEP_GPP<-Flux_High[Flux_High$Type_Flux %in% c("NEP_GPP"),]
+
+
+#2. Model NEP and carbon stocs
+source("Function/C_Model.R")
+nyears=max(GPP$Stand_Age)
+GPP <- GPP[order(GPP$Stand_Age),]
+GPP_Flux<-GPP$values
+
 ##GPP[1:20] <- seq(0,1000,length.out = 20) # or ramp up GPP
 NPPeff <- 0.5
 kcBio <- rep(0.05, nyears)
@@ -7,11 +42,11 @@ kCsoil1 <- rep(0.2, nyears)
 kCsoil2 <- rep(0.01, nyears)
 h=0.3
 #Assume steady state before: mini spin up or analytical (for each pool input/rate_constant)
-CbioSS <- mean(GPP)*NPPeff/mean(kCBio) #not enitrely correct if kCBio varies
-Csoil1SS <- CbioSS*mean(kCBio)/mean(kCsoil1)
+CbioSS <- mean(GPP_Flux)*NPPeff/mean(kcBio) #not enitrely correct if kCBio varies
+Csoil1SS <- CbioSS*mean(kcBio)/mean(kCsoil1)
 Csoil2SS <- Csoil1SS*h*mean(kCsoil1)/mean(kCsoil2)
 
-Cdyn1 <- simpleCdyn(GPP, NPPeff,kcBio , kCsoil1, kCsoil2, h, 0, Csoil1SS, Csoil2SS)
+Cdyn1 <- simpleCdyn(GPP_Flux, NPPeff,kcBio , kCsoil1, kCsoil2, h, 0, Csoil1SS, Csoil2SS)
 par(mfrow=c(2,1), mar=c(2,4,1,1)+0.1)
 
 with(Cdyn1, {
@@ -24,31 +59,4 @@ lines(time, Csoil2, col="brown", lwd=2)
 
 })
 
-
-
-simpleCdyn <- function (GPP, NPPeff, kCbio, kCsoil1, kCsoil2, h, CbioIni, Csoil1Ini, Csoil2Ini ) {
-  
-  n <- length(GPP)
-  NPP <- rep(NA, n)
-  Cbio <- rep(CbioIni, n)
-  Csoil1 <- rep(Csoil1Ini, n)
-  Csoil2 <- rep(Csoil2Ini, n)
-  Ctot <- Cbio + Csoil1 + Csoil2
-  NEP <- rep(NA, n)
-  NEP_flux <- rep(NA, n)
-  
-  for (i in 1:(n-1)) {
-  NPP[i] <- NPPeff * GPP[i]
-  Cbio[i+1]  <- Cbio[i] + NPP[i] - kCbio[i]*Cbio[i]
-  Csoil1[i+1] <- Csoil1[i] - kCsoil1[i]*Csoil1[i] +  kCbio[i]*Cbio[i]
-  Csoil2[i+1] <- Csoil2[i] - kCsoil2[i]*Csoil2[i] + h*kCsoil1[i]*Csoil1[i]
-  
-  Ctot[i+1] <- Cbio[i+1]+Csoil1[i+1]+Csoil2[i+1]
-  NEP[i] <- Ctot[i+1]-Ctot[i] #Stock based
-  NEP_flux[i] <- NPP[i] -  kCsoil2[i]*Csoil2[i] - (1-h)*kCsoil1[i]*Csoil1[i] #Flux based (should be the same)
-  
-  }
-  allinfo=data.frame(time=1:n, NEP=NEP, NEP_flux=NEP_flux, Ctot=Ctot, Cbio=Cbio, Csoil1=Csoil1, Csoil2=Csoil2)
-  allinfo
-}
 
