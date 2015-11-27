df41e14701767f07c2b7f9a849db1163137952d5
SimonBesnard <simon.besnard@wur.nl>
2015-11-27 16:33:31 +0100

    Include GPPmax

--- Scripts/Model_NEP.R
+++ Scripts/Model_NEP.R
@@ -3,54 +3,43 @@
 ## 12.10.2015
 ###################################
 ## Load the necessary packages
+library(ggplot2)
+library(gridExtra)
 
-# Import dataframe
+# Compute GPP steady state
 dfAll_Sites<-readRDS("Output/df_Annual_Flux.rds")
-
-#1. Subset dataset
 Flux_High<-dfAll_Sites[dfAll_Sites$Study %in% c("Yes"),]
+GPP<- Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
+params <- c(GPP15= 1923, GPP1000=1827, a1=242, a2=0.049, k=-0.00025)
+GPP$GPPmat<-with(as.list(params), GPP15*((1+exp(a1-a2*15))/(1+exp(a1-a2*GPP$Tair))))
+GPP$GPPp<-with(as.list(params), GPP1000*((1-exp(-k*GPP$Annual_Preci))/(1-exp(-k*1000))))
+GPP<-transform(GPP, GPPmax = pmin(GPPmat, GPPp))
 
-#Subset type of flux
-
-#NEP
-NEP<-Flux_High[Flux_High$Type_Flux %in% c("NEP"),]
-
-#GPP
-GPP<-Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
-
-#Reco
-Reco<-Flux_High[Flux_High$Type_Flux %in% c("Respiration"),]
-
-#Ratio GPP-Reco
-Ratio_GPP_ER<-Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
-
-#Ratio NEP-GPP
-Ratio_NEP_GPP<-Flux_High[Flux_High$Type_Flux %in% c("NEP_GPP"),]
-
-#2. Model NEP and carbon stocks
+# Create GPP dataframe
 source("Function/C_Model.R")
-nyears=max(GPP$Stand_Age)
-GPP <- GPP[order(GPP$Stand_Age),]
+nyears=300
+GPP_Flux<- as.data.frame(seq(0:300))
+colnames(GPP_Flux)<- "Age"
 f_Age_GPP<- function (x) {1287.1921959 *(1-exp(-0.1343273*x))}
-GPP$f_Age<- f_Age_GPP(GPP$Stand_Age)
-GPP_Flux<-GPP$f_Age
-plot(GPP$Stand_Age, GPP$f_Age)
+GPP_Flux$f_Age<- f_Age_GPP(GPP_Flux$Age)
+GPP_Flux<-GPP_Flux$f_Age
 
-##GPP[1:20] <- seq(0,1000,length.out = 20) # or ramp up GPP
+# Set parameters values of the model
 NPPeff <- 0.5
 kcBio <- rep(0.05, nyears)
 kCsoil1 <- rep(0.2, nyears)
 kCsoil2 <- rep(0.01, nyears)
 h=0.3
 #Assume steady state before: mini spin up or analytical (for each pool input/rate_constant)
-CbioSS <- mean(GPP_Flux)*NPPeff/mean(kcBio) #not enitrely correct if kCBio varies
+CbioSS <- mean(GPP$GPPmax)*NPPeff/mean(kcBio) #not enitrely correct if kCBio varies
 Csoil1SS <- CbioSS*mean(kcBio)/mean(kCsoil1)
 Csoil2SS <- Csoil1SS*h*mean(kCsoil1)/mean(kCsoil2)
-
+CtotSS<- CbioSS + Csoil1SS +  Csoil2SS
+# Run the model
 Cdyn1 <- simpleCdyn(GPP_Flux, NPPeff,kcBio , kCsoil1, kCsoil2, h, 0, Csoil1SS, Csoil2SS)
 
+# Plot outputs
 gg1<-ggplot(data = Cdyn1, aes(x = time, y = NEP)) +
-  geom_point()+
   geom_line()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Stand Age (year)")+
@@ -66,8 +55,12 @@ gg2<-ggplot(data = Cdyn1) +
   ylab("Carbon stock")+
   scale_colour_manual("",breaks = c("Ctot", "Cbio", "Csoil1", "Csoil2"),
                       values=c("darkgreen", "blue","brown", "black"), 
-                      labels=c("Total Carbon", "Above-ground", "Below-ground", "Deadwood"))
-
+                      labels=c("Total Carbon", "Above-ground", "Below-ground", "Deadwood"))+ 
+  geom_hline(yintercept=11413.33, colour="darkgreen", linetype="dotted")+
+  geom_hline(yintercept=31386.67, colour="black", linetype="dotted")+
+  geom_hline(yintercept=2853.334, colour="blue", linetype="dotted")+
+  geom_hline(yintercept=17120, colour="brown", linetype="dotted")
+  
 pdf("Latex/Figures/NEP_Model.eps", width = 8.87, height = 5.48) # Open a new pdf file
 grid.arrange(gg1, gg2, nrow=2) # Write the grid.arrange in the file
 dev.off() # Close the file
