ab1d735ef27b378017845034105cb04e77a30779
SimonBesnard <simon.besnard@wur.nl>
2015-12-08 18:20:32 +0100

    Include GPP/GPPmax

--- Scripts/Eco_Response_Fit.R
+++ Scripts/Eco_Response_Fit.R
@@ -56,6 +56,19 @@ Ratio_NEP_GPPmax<- Ratio_NEP_GPPmax[c("Site_ID", "year", "Stand_Replacement", "I
 colnames(Ratio_NEP_GPPmax)<-c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "Type_Flux", "values", "Annual_Preci", 
                          "Tair", "Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")
 
+#Compute ratio GPP/GPPmax
+GPP$GPP_GPPmax<- GPP$values/GPP$GPPmax
+Ratio_GPP_GPPmax<- GPP
+Ratio_GPP_GPPmax<-Ratio_GPP_GPPmax[, !(colnames(Ratio_GPP_GPPmax) %in% c("values", "Type_Flux", "GPPmat", "GPPmax", "GPPp", "NEP_GPPmax"))]
+Ratio_GPP_GPPmax<-gather(Ratio_GPP_GPPmax, variable, values, -Annual_Preci, -year, 
+                         -Ecosystem, -Climate, -Disturbance,
+                         -Stand_Age, -Site_ID, -Stand_Replacement, -Int_Replacement,
+                         -Tair, -Rg, -Study, -Lat, -Long)
+Ratio_GPP_GPPmax<- Ratio_GPP_GPPmax[c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "variable", "values", "Annual_Preci", 
+                                      "Tair","Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")]
+colnames(Ratio_GPP_GPPmax)<-c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "Type_Flux", "values", "Annual_Preci", 
+                              "Tair", "Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")
+
 # Create dataset mean per site
 NEP_Mean_Site<-ddply(NEP, .(Site_ID, Type_Flux),
                           summarise,
@@ -99,6 +112,13 @@ Ratio_NEP_GPPmax_Mean_Site<-ddply(Ratio_NEP_GPPmax, .(Site_ID, Type_Flux),
                                 Annual_Preci=mean(Annual_Preci, na.rm=T),
                                 Tair=mean(Tair, na.rm=T))
 
+Ratio_GPP_GPPmax_Mean_Site<-ddply(Ratio_GPP_GPPmax, .(Site_ID, Type_Flux),
+                                  summarise,
+                                  Stand_Age= median(Stand_Age, na.rm=T),
+                                  values=mean(values, na.rm=T),
+                                  Annual_Preci=mean(Annual_Preci, na.rm=T),
+                                  Tair=mean(Tair, na.rm=T))
+
 # 1.2 Test the best fitting function to ecosystem response - Modelling efficiency (Nash-Sutcliffe Efficiency test)
 
 # Load function 
@@ -108,6 +128,7 @@ source("Function/NSE_Reco.R")
 source("Function/NSE_Ratio_GPP_ER.R")
 source("Function/NSE_Ratio_NEP_GPP.R")
 source("Function/NSE_Ratio_NEP_GPPmax.R")
+source("Function/NSE_Ratio_GPP_GPPmax.R")
 
 # Create a list of dataframe
 df.list1<-list(GPP, GPP_Mean_Site)
@@ -116,6 +137,7 @@ df.list3<-list(Reco, Reco_Mean_Site)
 df.list4<-list(Ratio_GPP_Reco, Ratio_GPP_Reco_Mean_Site)
 df.list5<-list(Ratio_NEP_GPP, Ratio_NEP_GPP_Mean_Site)
 df.list6<-list(Ratio_NEP_GPPmax, Ratio_NEP_GPPmax_Mean_Site)
+df.list7<-list(Ratio_GPP_GPPmax, Ratio_GPP_GPPmax_Mean_Site)
 
 Out1<- lapply(df.list1, stat_GPP)
 Out2<- lapply(df.list2, stat_NEP)
@@ -123,6 +145,7 @@ Out3<- lapply(df.list3, stat_Reco)
 Out4<- lapply(df.list4, stat_Ratio_GPP_ER)
 Out5<- lapply(df.list5, stat_Ratio_NEP_GPP)
 Out6<- lapply(df.list6, stat_Ratio_NEP_GPPmax)
+Out7<- lapply(df.list7, stat_Ratio_GPP_GPPmax)
 
 # 2. Plot ecosystem response with the best fit function with all years per site
 source("Function/CI_Est.R")
@@ -264,6 +287,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_GPP_Reco$Annual_Preci[Ratio_GPP_Reco$Annual_Preci == 0] <- NA
 gg4<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_GPP_Reco, !is.na(Annual_Preci)), aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_GPP_Reco, is.na(Annual_Preci)), aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -280,8 +304,7 @@ gg4<-ggplot(predVals, aes(x, lower, upper)) +
         legend.position="none", 
         legend.box="horizontal") +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
-         size = guide_legend(title.position="top", title.hjust = 0.5))+
-  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)
+         size = guide_legend(title.position="top", title.hjust = 0.5))
 
 # 2.5 Ratio NEP/GPP
 
@@ -304,6 +327,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_NEP_GPP$Annual_Preci[Ratio_NEP_GPP$Annual_Preci == 0] <- NA
 gg5<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_NEP_GPP, !is.na(Annual_Preci)), aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_NEP_GPP, is.na(Annual_Preci)), aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -320,8 +344,7 @@ gg5<-ggplot(predVals, aes(x, lower, upper)) +
         legend.position="none", 
         legend.box="horizontal") +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
-         size = guide_legend(title.position="top", title.hjust = 0.5))+
-  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)
+         size = guide_legend(title.position="top", title.hjust = 0.5))
 
 # 2.6 Ratio NEP/GPPclimax 
 
@@ -343,6 +366,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_NEP_GPPmax$Annual_Preci[Ratio_NEP_GPPmax$Annual_Preci == 0] <- NA
 gg6<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_NEP_GPPmax, !is.na(Annual_Preci)),  aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_NEP_GPPmax, is.na(Annual_Preci)),  aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -361,7 +385,46 @@ gg6<-ggplot(predVals, aes(x, lower, upper)) +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
          size = guide_legend(title.position="top", title.hjust = 0.5))
 
-# 2.7.Plot all carbon fluxes plots together
+# 2.6 Ratio GPP/GPPclimax 
+
+# Compute the best fit function
+Fun_Ratio_GPP_GPPmax<-nlsLM(values~A*(1-exp(k*Stand_Age)), data = Ratio_GPP_GPPmax, 
+                            start = list(A= 0.6575, k= -0.1007), control = list(maxiter = 500))
+# Calculate the confidence interval
+predCI <- predict(as.lm.nls(Fun_Ratio_GPP_GPPmax), interval = 'confidence', level = 0.95)
+
+# Make the predictions on our defined x
+x <- seq(min(Ratio_GPP_GPPmax$Stand_Age),max(Ratio_GPP_GPPmax$Stand_Age),length=50)
+pred1 <- approx(Ratio_GPP_GPPmax$Stand_Age, predCI[, 1], xout = x) ## fitted values
+pred2 <- approx(Ratio_GPP_GPPmax$Stand_Age, predCI [, 2], xout = x) ## lower CI
+pred3 <- approx(Ratio_GPP_GPPmax$Stand_Age, predCI[, 3], xout = x) ## upper CI
+
+# Put this into a data frame
+predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
+
+# Plot using ggplot
+Ratio_GPP_GPPmax$Annual_Preci[Ratio_GPP_GPPmax$Annual_Preci == 0] <- NA
+gg7<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)+
+  geom_point(data = subset(Ratio_GPP_GPPmax, !is.na(Annual_Preci)), aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
+  geom_point(data = subset(Ratio_GPP_GPPmax, is.na(Annual_Preci)), aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
+  geom_line(aes(y = fit), colour="black", size=0.8)+
+  geom_line(mapping = aes(y = upper), lty = "dashed", size=0.8) +
+  geom_line(mapping = aes(y = lower), lty = "dashed", size=0.8)+
+  scale_colour_gradient(low="#00FF33", high ="#FF0000")+
+  labs(colour="Annual air temperature (°C)", size="Annual precipitation (mm.y-1)")+
+  xlab("Stand age") + ylab("Ratio GPP-Reco")+ 
+  ylim(0,3)+
+  scale_size(range = c(4, 9)) +
+  theme_bw(base_size = 12, base_family = "Helvetica") + 
+  theme(panel.grid.minor = element_line(colour="grey", size=0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position="none", 
+        legend.box="horizontal") +
+  guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
+         size = guide_legend(title.position="top", title.hjust = 0.5))
+
+# 2.8. Plot all carbon fluxes plots together
 
 # Create an arrange plot object
 pdf("Latex/Figures/Annual_Flux_All_Years.eps", width = 15, height = 12) # Open a new pdf file
@@ -512,6 +575,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_GPP_Reco_Mean_Site$Annual_Preci[Ratio_GPP_Reco_Mean_Site$Annual_Preci == 0] <- NA
 gg4<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_GPP_Reco_Mean_Site, !is.na(Annual_Preci)),  aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_GPP_Reco_Mean_Site, is.na(Annual_Preci)),  aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -528,8 +592,7 @@ gg4<-ggplot(predVals, aes(x, lower, upper)) +
         legend.position="none", 
         legend.box="horizontal") +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
-         size = guide_legend(title.position="top", title.hjust = 0.5))+
-  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)
+         size = guide_legend(title.position="top", title.hjust = 0.5))
 
 # 3.5 Ratio NEP/GPP
 
@@ -552,6 +615,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_NEP_GPP_Mean_Site$Annual_Preci[Ratio_NEP_GPP_Mean_Site$Annual_Preci == 0] <- NA
 gg5<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_NEP_GPP_Mean_Site, !is.na(Annual_Preci)),  aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_NEP_GPP_Mean_Site, is.na(Annual_Preci)),  aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -568,8 +632,7 @@ gg5<-ggplot(predVals, aes(x, lower, upper)) +
         legend.position="none", 
         legend.box="horizontal") +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
-         size = guide_legend(title.position="top", title.hjust = 0.5))+
-  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)
+         size = guide_legend(title.position="top", title.hjust = 0.5))
 
 # 3.6 Ratio NEP/GPPclimax 
 
@@ -592,6 +655,7 @@ predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
 # Plot using ggplot
 Ratio_NEP_GPPmax_Mean_Site$Annual_Preci[Ratio_NEP_GPPmax_Mean_Site$Annual_Preci == 0] <- NA
 gg6<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=0, colour='grey', lty="dashed", size=0.8)+
   geom_point(data = subset(Ratio_NEP_GPPmax_Mean_Site, !is.na(Annual_Preci)),  aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
   geom_point(data = subset(Ratio_NEP_GPPmax_Mean_Site, is.na(Annual_Preci)),  aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
   geom_line(aes(y = fit), colour="black", size=0.8)+
@@ -610,7 +674,46 @@ gg6<-ggplot(predVals, aes(x, lower, upper)) +
   guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
          size = guide_legend(title.position="top", title.hjust = 0.5))
 
-# 3.7.Plot all carbon fluxes plots together
+# 3.7 Ratio GPP/GPPclimax 
+
+# Compute the best fit function
+Fun_Ratio_GPP_GPPmax_Mean_Site<-nlsLM(values~A*(1-exp(k*Stand_Age)), data = Ratio_GPP_GPPmax_Mean_Site, 
+                            start = list(A= 0.6575, k= -0.1007), control = list(maxiter = 500))
+# Calculate the confidence interval
+predCI <- predict(as.lm.nls(Fun_Ratio_GPP_GPPmax_Mean_Site), interval = 'confidence', level = 0.95)
+
+# Make the predictions on our defined x
+x <- seq(min(Ratio_GPP_GPPmax_Mean_Site$Stand_Age),max(Ratio_GPP_GPPmax_Mean_Site$Stand_Age),length=50)
+pred1 <- approx(Ratio_GPP_GPPmax_Mean_Site$Stand_Age, predCI[, 1], xout = x) ## fitted values
+pred2 <- approx(Ratio_GPP_GPPmax_Mean_Site$Stand_Age, predCI [, 2], xout = x) ## lower CI
+pred3 <- approx(Ratio_GPP_GPPmax_Mean_Site$Stand_Age, predCI[, 3], xout = x) ## upper CI
+
+# Put this into a data frame
+predVals <- data.frame(x=x, fit=pred1$y,lower=pred2$y,upper=pred3$y)
+
+# Plot using ggplot
+Ratio_GPP_GPPmax_Mean_Site$Annual_Preci[Ratio_GPP_GPPmax_Mean_Site$Annual_Preci == 0] <- NA
+gg7<-ggplot(predVals, aes(x, lower, upper)) +
+  geom_hline(yintercept=1, colour='grey', lty="dashed", size=0.8)+
+  geom_point(data = subset(Ratio_GPP_GPPmax_Mean_Site, !is.na(Annual_Preci)), aes(x = Stand_Age, y = values, size=Annual_Preci, colour=Tair), inherit.aes = FALSE)+
+  geom_point(data = subset(Ratio_GPP_GPPmax_Mean_Site, is.na(Annual_Preci)), aes(x = Stand_Age, y = values), colour='grey50', inherit.aes = FALSE)+
+  geom_line(aes(y = fit), colour="black", size=0.8)+
+  geom_line(mapping = aes(y = upper), lty = "dashed", size=0.8) +
+  geom_line(mapping = aes(y = lower), lty = "dashed", size=0.8)+
+  scale_colour_gradient(low="#00FF33", high ="#FF0000")+
+  labs(colour="Annual air temperature (°C)", size="Annual precipitation (mm.y-1)")+
+  xlab("Stand age") + ylab("Ratio GPP-Reco")+ 
+  ylim(0,3)+
+  scale_size(range = c(4, 9)) +
+  theme_bw(base_size = 12, base_family = "Helvetica") + 
+  theme(panel.grid.minor = element_line(colour="grey", size=0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position="none", 
+        legend.box="horizontal") +
+  guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
+         size = guide_legend(title.position="top", title.hjust = 0.5))
+
+# 3.8.Plot all carbon fluxes plots together
 
 # Create an arrange plot object
 pdf("Latex/Figures/Annual_Flux_Mean_Site.eps", width = 15, height = 12) # Open a new pdf file
