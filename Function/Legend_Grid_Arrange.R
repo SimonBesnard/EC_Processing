8cdd4a07ff069cd7dfa6ff19ff6f31a20153b9d2
SimonBesnard <simon.besnard@wur.nl>
2016-01-14 16:22:54 +0100

    Function to add legend to an arrange grid object

---
+++ Function/Legend_Grid_Arrange.R
@@ -0,0 +1,12 @@
+grid_arrange_shared_legend <- function(...) {
+  plots <- list(...)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  grid.arrange(
+    do.call(arrangeGrob, lapply(plots, function(x)
+      x + theme(legend.position="none"))),
+    legend,
+    ncol = 1,
+    heights = unit.c(unit(1, "npc") - lheight, lheight))
+}
