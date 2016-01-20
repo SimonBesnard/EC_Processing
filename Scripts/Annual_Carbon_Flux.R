9e4dd60bb054e4829981ab02ab70d60bb3209e70
SimonBesnard <simon.besnard@wur.nl>
2016-01-20 17:14:00 +0100

    Update climate data

--- Scripts/Annual_Carbon_Flux.R
+++ Scripts/Annual_Carbon_Flux.R
@@ -87,73 +87,11 @@ for (i in seq_along(Sum_Sd_Flux)){
 #Combine the flux sites in one dataframe
 dfAll_Sites<- do.call("rbind", Sum_Sd_Flux)
 
-# Add climate data to the dataframe
-
-#CA-NS1
-dfAll_Sites[c(62,63), "Annual_Preci"]<- 500
-
-#CA-NS3
-dfAll_Sites[71, "Annual_Preci"]<- 502
-
-# CA-NS5
-dfAll_Sites[77, "Annual_Preci"]<- 500
-
-#CA-SJ1
-dfAll_Sites[133, "Annual_Preci"]<- 430
-
-#CA-SJ2
-dfAll_Sites[138, "Annual_Preci"]<- 430
-
-#CA-TP1
-dfAll_Sites[143, "Annual_Preci"]<- 1036
-
-#CA-TP2
-dfAll_Sites[c(145:147), "Annual_Preci"]<- 1036
-
-#CZ-Bk1
-dfAll_Sites[c(154:157, 159:160), "Annual_Preci"]<- 1026
-
-#FI-Sod
-dfAll_Sites[c(212: 213), "Annual_Preci"]<- 525
-
-#Il-Yat
-dfAll_Sites[c(244:245), "Annual_Preci"]<- 277
-
-#It-Col
-dfAll_Sites[c(251:253), "Annual_Preci"]<- 971
-
-#It-Lav
-dfAll_Sites[c(259:260), "Annual_Preci"]<- 757
-
-#It-Sro
-dfAll_Sites[c(283, 285), "Annual_Preci"]<- 898
-
-#JP-Tak
-dfAll_Sites[c(291:296), "Annual_Preci"]<- 1024
-
-#Ru-Fyo
-dfAll_Sites[c(321:322), "Annual_Preci"]<- 671
-
-#SE-Nor
-dfAll_Sites[c(342,346), "Annual_Preci"]<- 561
-
-#SE-Sk1
-dfAll_Sites[349, "Annual_Preci"]<- 567
-
-#SE-Sk2
-dfAll_Sites[c(350:351), "Annual_Preci"]<- 573
-
-#US-Lph
-dfAll_Sites[417, "Annual_Preci"]<- 1071
-
-#US-Syv
-dfAll_Sites[473, "Annual_Preci"]<- 826
-
 # Remove disturbed years
 # sites: AU-Tum (2003: insect outbreak), BE-Bra (2000: thinning), CA-Ca2 (2001: forest not planted yet), 
 #CA-SJ2 (2004: outlier for GPP values), NL-Loo (2006: storm), SE-Nor (2005: thinning occured this year), UK-Gri (2006:Thinning)
 # US-Nc2 (2006: insect attack), US-SO2 (2004: wildfire), US-SP1 (2005: storm), US-Wcr (2002: insect attack)
-dfAll_Sites<-dfAll_Sites[-c(3, 13, 37, 139, 313, 348, 362, 423, 438, 456, 482),]
+dfAll_Sites<-dfAll_Sites[-c(3, 13, 37, 139, 345, 380, 394, 455, 470, 488, 514),]
 
 # Remove years missing in each flux site
 dfAll_Sites = dfAll_Sites[!is.na(dfAll_Sites$mean_Uncert),]
@@ -163,6 +101,48 @@ dfAll_Sites<- dfAll_Sites[dfAll_Sites$mean_Uncert>0.80,]
 
 # Remove uncomplete data collection within year. 
 dfAll_Sites<- dfAll_Sites[!dfAll_Sites$GPP==0,]
+
+# Add climate data to the dataframe
+rownames(dfAll_Sites) <- seq(length=nrow(dfAll_Sites)) 
+
+#CA-NS1
+dfAll_Sites[c(31), "Annual_Preci"]<- 500
+
+#CA-NS3
+dfAll_Sites[35, "Annual_Preci"]<- 502
+
+#CZ-Bk1
+dfAll_Sites[c(80), "Annual_Preci"]<- 1026
+
+#FI-Sod
+dfAll_Sites[c(134), "Annual_Preci"]<- 525
+
+#Il-Yat
+dfAll_Sites[c(163,164), "Annual_Preci"]<- 277
+
+#It-Col
+dfAll_Sites[c(167), "Annual_Preci"]<- 971
+
+#It-Lav
+dfAll_Sites[c(174:175), "Annual_Preci"]<- 757
+
+#It-Sro
+dfAll_Sites[c(191, 193), "Annual_Preci"]<- 898
+
+#JP-Tak
+dfAll_Sites[c(198:203), "Annual_Preci"]<- 1024
+
+#Ru-Fyo
+dfAll_Sites[c(217:218), "Annual_Preci"]<- 671
+
+#SE-Nor
+dfAll_Sites[c(230:231), "Annual_Preci"]<- 561
+
+#SE-Sk1
+dfAll_Sites[232, "Annual_Preci"]<- 567
+
+#SE-Sk2
+dfAll_Sites[c(233:234), "Annual_Preci"]<- 573
 
 # Restructure dataframe
 dfAll_Sites<-gather(dfAll_Sites, variable, value, -Annual_Preci, -year, 
@@ -194,5 +174,89 @@ colnames(Glob_Forest_df)<-c("Site_ID", "year", "Stand_Replacement", "Int_Replace
                          "Tair", "Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")
 dfAll_Sites<-rbind(dfAll_Sites, Glob_Forest_df)
 
-# Save dataframe in a rds file
+# 4. Create dataframe for post-processing
+
+# 4.1 Subset original data set into fluxes
+Flux_High<-dfAll_Sites[dfAll_Sites$Study %in% c("Yes"),]
+NEP<- Flux_High[Flux_High$Type_Flux %in% c("NEP"),]
+GPP<- Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
+Reco<- Flux_High[Flux_High$Type_Flux %in% c("Respiration"),]
+Ratio_NEP_GPP<- Flux_High[Flux_High$Type_Flux %in% c("NEP_GPP"),]
+Ratio_GPP_Reco<- Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
+
+#Compute GPPclimax based on the lieth model
+params <- c(GPP15= 1923, GPP1000=1827, a1=242, a2=0.049, k=-0.00025)
+GPP$GPPmat<-with(as.list(params), GPP15*((1+exp(a1-a2*15))/(1+exp(a1-a2*GPP$Tair))))
+GPP$GPPp<-with(as.list(params), GPP1000*((1-exp(-k*GPP$Annual_Preci))/(1-exp(-k*1000))))
+GPP<-transform(GPP, GPPmax = pmin(GPPmat, GPPp))
+
+# Compute ratio NEP-GPPclimx ratio
+GPP$NEP_GPPmax<- NEP$values/GPP$GPPmax
+Ratio_NEP_GPPmax<- GPP
+Ratio_NEP_GPPmax<-Ratio_NEP_GPPmax[, !(colnames(Ratio_NEP_GPPmax) %in% c("values", "Type_Flux", "GPPmat", "GPPmax", "GPPp"))]
+Ratio_NEP_GPPmax<-gather(Ratio_NEP_GPPmax, variable, values, -Annual_Preci, -year, 
+                         -Ecosystem, -Climate, -Disturbance,
+                         -Stand_Age, -Site_ID, -Stand_Replacement, -Int_Replacement,
+                         -Tair, -Rg, -Study, -Lat, -Long)
+Ratio_NEP_GPPmax<- Ratio_NEP_GPPmax[c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "variable", "values", "Annual_Preci", 
+                                      "Tair","Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")]
+colnames(Ratio_NEP_GPPmax)<-c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "Type_Flux", "values", "Annual_Preci", 
+                              "Tair", "Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")
+
+# Create average site dataframe
+NEP_Mean_Site<-ddply(NEP, .(Site_ID, Type_Flux),
+                     summarise,
+                     Stand_Age= mean(Stand_Age, na.rm=T),
+                     values=mean(values, na.rm=T),
+                     Annual_Preci=mean(Annual_Preci, na.rm=T),
+                     Tair=mean(Tair, na.rm=T))
+
+GPP_Mean_Site<-ddply(GPP, .(Site_ID, Type_Flux),
+                     summarise,
+                     Stand_Age= mean(Stand_Age, na.rm=T),
+                     values=mean(values, na.rm=T),
+                     Annual_Preci=mean(Annual_Preci, na.rm=T),
+                     Tair=mean(Tair, na.rm=T))
+
+Reco_Mean_Site<-ddply(Reco, .(Site_ID, Type_Flux),
+                      summarise,
+                      Stand_Age= mean(Stand_Age, na.rm=T),
+                      values=mean(values, na.rm=T),
+                      Annual_Preci=mean(Annual_Preci, na.rm=T),
+                      Tair=mean(Tair, na.rm=T))
+
+Ratio_NEP_GPP_Mean_Site<-ddply(Ratio_NEP_GPP, .(Site_ID, Type_Flux),
+                               summarise,
+                               Stand_Age= mean(Stand_Age, na.rm=T),
+                               values=mean(values, na.rm=T),
+                               Annual_Preci=mean(Annual_Preci, na.rm=T),
+                               Tair=mean(Tair, na.rm=T))
+
+Ratio_GPP_Reco_Mean_Site<-ddply(Ratio_GPP_Reco, .(Site_ID, Type_Flux),
+                                summarise,
+                                Stand_Age= mean(Stand_Age, na.rm=T),
+                                values=mean(values, na.rm=T),
+                                Annual_Preci=mean(Annual_Preci, na.rm=T),
+                                Tair=mean(Tair, na.rm=T))
+
+Ratio_NEP_GPPmax_Mean_Site<-ddply(Ratio_NEP_GPPmax, .(Site_ID, Type_Flux),
+                                  summarise,
+                                  Stand_Age= median(Stand_Age, na.rm=T),
+                                  values=mean(values, na.rm=T),
+                                  Annual_Preci=mean(Annual_Preci, na.rm=T),
+                                  Tair=mean(Tair, na.rm=T))
+
+# Save all dataframe in a rds files
 saveRDS(dfAll_Sites, file="Output/df_Annual_Flux.rds")
+saveRDS(NEP, file="Output/NEP.rds")
+saveRDS(NEP_Mean_Site, file="Output/NEP_Mean_Site.rds")
+saveRDS(GPP, file="Output/GPP.rds")
+saveRDS(GPP_Mean_Site, file="Output/GPP_Mean_Site.rds")
+saveRDS(Reco, file="Output/Reco.rds")
+saveRDS(Reco_Mean_Site, file="Output/Reco_Mean_Site.rds")
+saveRDS(Ratio_GPP_Reco, file="Output/Ratio_GPP_Reco.rds")
+saveRDS(Ratio_GPP_Reco_Mean_Site, file="Output/Ratio_GPP_Reco_Mean_Site.rds")
+saveRDS(Ratio_NEP_GPP, file="Output/Ratio_NEP_GPP.rds")
+saveRDS(Ratio_NEP_GPP_Mean_Site, file="Output/Ratio_NEP_GPP_Mean_Site.rds")
+saveRDS(Ratio_NEP_GPPmax, file="Output/Ratio_NEP_GPPmax.rds")
+saveRDS(Ratio_NEP_GPPmax_Mean_Site, file="Output/Ratio_NEP_GPPmax_Mean_Site.rds")
