c90171096579478b6a421445737ed73f39300dc1
SimonBesnard <simon.besnard@wur.nl>
2016-01-20 17:15:00 +0100

    Import dataframe

--- Scripts/Model_NEP.R
+++ Scripts/Model_NEP.R
@@ -3,7 +3,6 @@
 ## 12.10.2015
 ###################################
 ## Load the necessary packages
-library(ggplot2)
 library(gridExtra)
 library (ggplot2)
 library(scales)
@@ -16,42 +15,20 @@ library(grid)
 
 # Import dataframe
 dfAll_Sites<-readRDS("Output/df_Annual_Flux.rds")
+NEP<-readRDS("Output/NEP.rds")
+NEP_Mean_Site<-readRDS("Output/NEP_Mean_Site.rds")
+GPP<-readRDS("Output/GPP.rds")
+GPP_Mean_Site<-readRDS("Output/GPP_Mean_Site.rds")
+Reco<-readRDS("Output/Reco.rds")
+Reco_Mean_Site<-readRDS("Output/Reco_Mean_Site.rds")
+Ratio_GPP_Reco<-readRDS("Output/Ratio_GPP_Reco.rds")
+Ratio_GPP_Reco_Mean_Site<-readRDS("Output/Ratio_GPP_Reco_Mean_Site.rds")
+Ratio_NEP_GPP<-readRDS("Output/Ratio_NEP_GPP.rds")
+Ratio_NEP_GPP_Mean_Site<- readRDS("Output/Ratio_NEP_GPP_Mean_Site.rds")
+Ratio_NEP_GPPmax<- readRDS("Output/Ratio_NEP_GPPmax.rds")
+Ratio_NEP_GPPmax_Mean_Site<- readRDS("Output/Ratio_NEP_GPPmax_Mean_Site.rds")
 
-# 1.1. Subset dataframe for fitting process 
-
-#Subset data set
-Flux_High<-dfAll_Sites[dfAll_Sites$Study %in% c("Yes"),]
-NEP<- Flux_High[Flux_High$Type_Flux %in% c("NEP"),]
-GPP<- Flux_High[Flux_High$Type_Flux %in% c("GPP"),]
-Reco<- Flux_High[Flux_High$Type_Flux %in% c("Respiration"),]
-Ratio_NEP_GPP<- Flux_High[Flux_High$Type_Flux %in% c("NEP_GPP"),]
-Ratio_GPP_Reco<- Flux_High[Flux_High$Type_Flux %in% c("GPP_ER"),]
-
-#Compute GPPmax based on the lieth model
-#Min model
-params <- c(GPP15= 1923, GPP1000=1827, a1=242, a2=0.049, k=-0.00025)
-GPP$GPPmat<-with(as.list(params), GPP15*((1+exp(a1-a2*15))/(1+exp(a1-a2*GPP$Tair))))
-GPP$GPPp<-with(as.list(params), GPP1000*((1-exp(-k*GPP$Annual_Preci))/(1-exp(-k*1000))))
-GPP<-transform(GPP, GPPmax = pmin(GPPmat, GPPp))
-#Multiplicative model
-# params <- c(a=-0.119, b=1.315, c=-0.000664)
-# GPP$GPPmat<-with(as.list(params), 1/(1+exp(a*GPP$Tair+b))) 
-# GPP$GPPp<-with(as.list(params),(1-exp(c*GPP$Annual_Preci)))
-# GPP<-transform(GPP, GPPmax = 3000*GPPmat*GPPp)
-GPP$GPP_GPPmax<- NEP$values/GPP$GPPmax
-Ratio_NEP_GPPmax<- GPP
-Ratio_NEP_GPPmax<-Ratio_NEP_GPPmax[, !(colnames(Ratio_NEP_GPPmax) %in% c("values", "Type_Flux", "GPPmat", "GPPmax", "GPPp"))]
-Ratio_NEP_GPPmax<-gather(Ratio_NEP_GPPmax, variable, values, -Annual_Preci, -year, 
-                         -Ecosystem, -Climate, -Disturbance,
-                         -Stand_Age, -Site_ID, -Stand_Replacement, -Int_Replacement,
-                         -Tair, -Rg, -Study, -Lat, -Long)
-Ratio_NEP_GPPmax<- Ratio_NEP_GPPmax[c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "variable", "values", "Annual_Preci", 
-                                      "Tair","Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")]
-colnames(Ratio_NEP_GPPmax)<-c("Site_ID", "year", "Stand_Replacement", "Int_Replacement", "Type_Flux", "values", "Annual_Preci", 
-                              "Tair", "Rg", "Stand_Age", "Disturbance", "Climate", "Ecosystem", "Study", "Lat", "Long")
-
-
-# 1.2 Create NEP model using different functions
+# 1.2 Create NEP model using coursolle function
 
 # 1 2.1 Create daframe with Age and GPPclimax per ecosystem
 
@@ -63,26 +40,19 @@ GPPmax<-ddply(GPP, "Climate",
 # Compute df
 NEP_model<- as.data.frame(0:320)
 colnames(NEP_model)<- c("Age")
-NEP_model$Af<- 2956.9084
-NEP_model$Am<- 3382.6741
-NEP_model$Bsh<- 480.4198
-NEP_model$Cfa<- 1498.7218
-NEP_model$Cfb<- 1356.5680
-NEP_model$Cfc<- 1083.7448
-NEP_model$Csa<- 1273.2122
-NEP_model$Dfb<- 1180.7800
-NEP_model$Dfc<- 744.1699
-NEP_model$Dwb<- 865.5914
+NEP_model$Tropical<- 3169
+NEP_model$Dry<- 480
+NEP_model$Temperate<- 1292
+NEP_model$Continental<- 934
 NEP_model<-gather(NEP_model, variable, values, -Age)
 colnames(NEP_model)<-c("Age", "Climate", "GPPmax")
 
 # 1.2.2 Compute NEP models
 
 # From NEP to GPP ratio
-cols <- c("Af"="#f46d43", "Am"="#d73027", "Bsh"="#000099", "Cfa"="#fdae61", "Cfb"="#fee090", "Cfc"="#abd9e9", "Csa"="#ffffbf", "Dfb"="#e0f3f8",
-          "Dfc"="#4575b4", "Dwb"="#74add1")
-NEP_model$NEP<- (0.19945826*(exp(-0.00517618*NEP_model$Age)) -1.51419788*(exp(-0.17645323*NEP_model$Age)))*NEP_model$GPPmax
-ggplot(data=NEP_model)+
+cols <- c("Tropical"="#d7191c", "Temperate"="#fdae61", "Continental"="#abd9e9", "Dry"="#2c7bb6")
+NEP_model$NEP<- (0.208727985*(exp(-0.004670439*NEP_model$Age)) -1.530989643*(exp(-0.176894536*NEP_model$Age)))*NEP_model$GPPmax
+gg1<- ggplot(data=NEP_model)+
   geom_line(aes(x=Age, y=NEP, colour=Climate))+
   geom_point(data=NEP, aes(x=Stand_Age,y=values, group= NULL),  shape=20, color="black", size=2)+
   theme_bw(base_size = 16, base_family = "Helvetica")+
@@ -92,12 +62,13 @@ ggplot(data=NEP_model)+
         legend.position="right", 
         legend.box="horizontal",
         axis.ticks.length=unit(-0.25, "cm"), axis.ticks.margin=unit(0.5, "cm"))+
-  scale_colour_manual("",breaks = c("Af", "Am", "Bsh", "Cfa", "Cfb", "Cfc", "Csa", "Dfb", "Dfc", "Dwb"),
+  scale_colour_manual("Bioclimate",breaks = c("Tropical", "Temperate", "Continental", "Dry"),
                       values=cols, 
-                      labels=c("Tropical rainforest", "Tropical Monsoon", "Hot steppe", "Humid sub-tropical", "Oceanic-warm summer",
-                               "Oceanic-cold summer", "Dry summer", "Warm summer continental-No DS", "Continental subarctic", 
-                               "Warm summer continental"))+
+                      labels=c("Tropical", "Temperate", "Continental", "Dry"))+
   ylim(-700, 1000)
+
+# save plot
+ggsave("Latex/Figures/NEP_Model.eps", height=12, width=12)
 
 #2. Compute C stocks and NEP dynamics using C pool model
 
