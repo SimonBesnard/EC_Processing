94aaac6ce96637242224b07e62d1425ce6eff283
SimonBesnard <simon.besnard@wur.nl>
2016-02-02 19:05:12 +0100

    Remove gross fluxes from the analysis

--- Scripts/Residual_Analysis.R
+++ Scripts/Residual_Analysis.R
@@ -11,7 +11,9 @@ library(gridExtra)
 # 1.1 NEP
 
 # Import dataframe
+GPP<-readRDS("Output/GPP.rds")
 NEP<-readRDS("Output/NEP.rds")
+NEP$GPP<- GPP$values
 
 # Compute stand age function
 Fun_NEP<-nlsLM(values~A*(exp(B*Stand_Age)) + C*(exp(D*Stand_Age)), data = NEP,
@@ -23,14 +25,16 @@ NEP$Res<- residuals(Fun_NEP)
 #Plot residuals
 R2_Tair<-(cor(residuals(Fun_NEP), NEP$Tair, use="pairwise.complete.obs"))^2
 R2_Preci<-(cor(residuals(Fun_NEP), NEP$Annual_Preci, use="pairwise.complete.obs"))^2
+R2_GPP<-lm(residuals(Fun_NEP)~ NEP$GPP + I(NEP$GPP^2))
+summary(R2_GPP)
 
 gg1 <- ggplot(data = NEP, aes(x = Tair, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Air temperature (°C)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.11", x = 23, y = -325) +
+  ylab("Residual NEP model")+
+  annotate("text", label = "R-squared = 0.14", x = 20, y = -275) +
   facet_wrap(~Type_Flux)
 
 gg2 <- ggplot(data = NEP, aes(x = Annual_Preci, y = Res)) +
@@ -38,87 +42,29 @@ gg2 <- ggplot(data = NEP, aes(x = Annual_Preci, y = Res)) +
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Precipitation (mm)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.04", x = 3200, y = -325) +
+  ylab("Residual NEP model")+
+  annotate("text", label = "R-squared = 0.02", x = 2900, y = -275) +
   facet_wrap(~Type_Flux)
 
-# 1.2 GPP
-
-# Import dataframe
-GPP<-readRDS("Output/GPP.rds")
-
-# Compute stand age function
-Fun_GPP<-nlsLM(values~A*(1-exp(k*Stand_Age)), data = GPP, 
-               start = list(A=1327.462, k=-0.134), control = list(maxiter = 500))
-
-# Compute residual
-GPP$Res<- residuals(Fun_GPP)
-
-#Plot residuals
-R2_Tair<-(cor(residuals(Fun_GPP), GPP$Tair, use="pairwise.complete.obs"))^2
-R2_Preci<-(cor(residuals(Fun_GPP), GPP$Annual_Preci, use="pairwise.complete.obs"))^2
-
-gg3 <- ggplot(data = GPP, aes(x = Tair, y = Res)) +
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
+gg3<- ggplot(data = NEP, aes(x = GPP, y = Res)) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Air temperature (°C)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.51", x = 23, y = -800) +
+  xlab("GPP (gC.m-2.y-1)")+
+  ylab("Residual NEP model")+
+  annotate("text", label = "R-squared = 0.25", x = 3200, y = -275) +
   facet_wrap(~Type_Flux)
 
-gg4 <- ggplot(data = GPP, aes(x = Annual_Preci, y = Res)) +
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
-  geom_point()+
-  theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Precipitation (mm)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.53", x = 3200, y = -800) +
-  facet_wrap(~Type_Flux)
-
-# 1.3 Reco
-
-# Import dataframe
-Reco<-readRDS("Output/Reco.rds")
-
-# Compute stand age function
-Fun_Reco<-nlsLM(values~A*(Stand_Age^B)*(exp(k*Stand_Age)), data = Reco, 
-                start = list(A = 407.499524, B =0.379616, k =-0.005368), control = list(maxiter = 500))
-
-# Compute residual
-Reco$Res<- residuals(Fun_Reco)
-
-#Plot residuals
-R2_Tair<-(cor(residuals(Fun_Reco), Reco$Tair, use="pairwise.complete.obs"))^2
-R2_Preci<-(cor(residuals(Fun_Reco), Reco$Annual_Preci, use="pairwise.complete.obs"))^2
-
-gg5 <- ggplot(data = Reco, aes(x = Tair, y = Res)) +
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
-  geom_point()+
-  theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Air temperature (°C)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.44", x = 23, y = -700) +
-  facet_wrap(~Type_Flux)
-
-gg6 <- ggplot(data = Reco, aes(x = Annual_Preci, y = Res)) +
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
-  geom_point()+
-  theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Precipitation (mm)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.54", x = 3200, y = -700) +
-  facet_wrap(~Type_Flux)
-
-# 1.4 Ratio GPP-Reco
+# 1.2 Ratio GPP-Reco
 
 #Import dataframe
 Ratio_GPP_Reco<-readRDS("Output/Ratio_GPP_Reco.rds")
 levels(Ratio_GPP_Reco$Type_Flux) <- c("NEE", "GPP", "Respiration", "NEP", "mean_Uncert", "Ratio GPP-Reco", "Ratio NEP-GPP", "x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4" )
+Ratio_GPP_Reco$GPP<- GPP$values
 
 #Compute stand age function
 Fun_Ratio_GPP_Reco<-nlsLM(values~A*(1-exp(k*Stand_Age)), data = Ratio_GPP_Reco, 
-                          start = list(A= 1.1635, k= -0.2284), control = list(maxiter = 500))
+                          start = list(A= 1.1582, k= -0.2312), control = list(maxiter = 500))
 
 # Compute residual
 Ratio_GPP_Reco$Res<- residuals(Fun_Ratio_GPP_Reco)
@@ -128,30 +74,42 @@ R2_Tair<-lm(residuals(Fun_Ratio_GPP_Reco)~ Ratio_GPP_Reco$Tair + I(Ratio_GPP_Rec
 summary(R2_Tair)
 R2_Preci<-lm(residuals(Fun_Ratio_GPP_Reco)~ Ratio_GPP_Reco$Annual_Preci + I(Ratio_GPP_Reco$Annual_Preci^2))
 summary(R2_Preci)
+R2_GPP<- lm(residuals(Fun_Ratio_GPP_Reco)~ Ratio_GPP_Reco$GPP + I(Ratio_GPP_Reco$GPP^2))
+summary(R2_GPP)
 
-gg7 <- ggplot(data = Ratio_GPP_Reco, aes(x = Tair, y = Res)) +
+gg4 <- ggplot(data = Ratio_GPP_Reco, aes(x = Tair, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Air temperature (°C)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.22", x = 23, y = -0.4) +
+  ylab("Residual GPP-ER model")+
+  annotate("text", label = "R-squared = 0.25", x = 20, y = -0.4) +
   facet_wrap(~Type_Flux)
 
-gg8 <- ggplot(data = Ratio_GPP_Reco, aes(x = Annual_Preci, y = Res)) +
+gg5 <- ggplot(data = Ratio_GPP_Reco, aes(x = Annual_Preci, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Precipitation (mm)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.06", x = 3200, y = -0.4) +
+  ylab("Residual GPP-ER model")+
+  annotate("text", label = "R-squared = 0.06", x = 2900, y = -0.4) +
   facet_wrap(~Type_Flux)
 
-# 1.5 Ratio NEP-GPP
+gg6<- ggplot(data = Ratio_GPP_Reco, aes(x = GPP, y = Res)) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
+  geom_point()+
+  theme_bw(base_size = 12, base_family = "Helvetica")+
+  xlab("GPP (gC.m-2.y-1)")+
+  ylab("Residual GPP-ER model")+
+  annotate("text", label = "R-squared = 0.13", x = 3200, y = -0.4) +
+  facet_wrap(~Type_Flux)
+
+# 1.3 Ratio NEP-GPP
 
 # Import dataframe
 Ratio_NEP_GPP<-readRDS("Output/Ratio_NEP_GPP.rds")
 levels(Ratio_NEP_GPP$Type_Flux) <- c("NEE", "GPP", "Respiration", "NEP", "mean_Uncert", "Ratio GPP-Reco", "Ratio NEP-GPP", "x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4")
+Ratio_NEP_GPP$GPP<- GPP$values
 
 # Compute stand age function
 Fun_Ratio_NEP_GPP<-nlsLM(values~A*(exp(B*Stand_Age)) + C*(exp(D*Stand_Age)), data = Ratio_NEP_GPP,
@@ -165,63 +123,37 @@ R2_Tair<-lm(residuals(Fun_Ratio_NEP_GPP)~ Ratio_NEP_GPP$Tair + I(Ratio_NEP_GPP$T
 summary(R2_Tair)
 R2_Preci<-lm(residuals(Fun_Ratio_NEP_GPP)~ Ratio_NEP_GPP$Annual_Preci + I(Ratio_NEP_GPP$Annual_Preci^2))
 summary(R2_Preci)
+R2_GPP<- lm(residuals(Fun_Ratio_NEP_GPP)~ Ratio_NEP_GPP$GPP + I(Ratio_NEP_GPP$GPP^2))
+summary(R2_GPP)
 
-gg9 <- ggplot(data = Ratio_NEP_GPP, aes(x = Tair, y = Res)) +
+gg7 <- ggplot(data = Ratio_NEP_GPP, aes(x = Tair, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Air temperature (°C)")+
   ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.22", x = 23, y = -0.55) +
+  annotate("text", label = "R-squared = 0.25", x = 20, y = -0.55) +
   facet_wrap(~Type_Flux)
 
-gg10 <- ggplot(data = Ratio_NEP_GPP, aes(x = Annual_Preci, y = Res)) +
+gg8 <- ggplot(data = Ratio_NEP_GPP, aes(x = Annual_Preci, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
   xlab("Precipitation (mm)")+
   ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.06", x = 3200, y = -0.55) +
+  annotate("text", label = "R-squared = 0.06", x = 2900, y = -0.55) +
   facet_wrap(~Type_Flux)
 
-# 1.6 Ratio NEP-GPPclimax
-
-# Import dataframe
-Ratio_NEP_GPPmax<- readRDS("Output/Ratio_NEP_GPPmax.rds")
-
-# Compute stand age function
-Fun_Ratio_NEP_GPPmax<-nlsLM(values~A*(exp(B*Stand_Age)) + C*(exp(D*Stand_Age)), data = Ratio_NEP_GPPmax,
-                            start = list(A=-0.741553, B= -0.116557, C=0.236692, D=-0.006249), control = list(maxiter = 500))
-
-# Compute residual
-Ratio_NEP_GPPmax$Res<- residuals(Fun_Ratio_NEP_GPPmax)
-
-#Plot residuals
-R2_Tair<-lm(residuals(Fun_Ratio_NEP_GPPmax)~ Ratio_NEP_GPPmax$Tair + I(Ratio_NEP_GPPmax$Tair^2))
-summary(R2_Tair)
-R2_Preci<-lm(residuals(Fun_Ratio_NEP_GPPmax)~ Ratio_NEP_GPPmax$Annual_Preci + I(Ratio_NEP_GPPmax$Annual_Preci^2))
-summary(R2_Preci)
-levels(Ratio_NEP_GPPmax$Type_Flux) <- c("Ratio NEP-GPPmax")
-
-gg11 <- ggplot(data = Ratio_NEP_GPPmax, aes(x = Tair, y = Res)) +
+gg9<- ggplot(data = Ratio_NEP_GPP, aes(x = GPP, y = Res)) +
   geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
   geom_point()+
   theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Air temperature (°C)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.17", x = 23, y = -0.5)+ 
+  xlab("GPP (gC.m-2.y-1)")+
+  ylab("Residual CUE model")+
+  annotate("text", label = "R-squared = 0.14", x = 3200, y = -0.55) +
   facet_wrap(~Type_Flux)
 
-gg12 <- ggplot(data = Ratio_NEP_GPPmax, aes(x = Annual_Preci, y = Res)) +
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x + I(x^2))+
-  geom_point()+
-  theme_bw(base_size = 12, base_family = "Helvetica")+
-  xlab("Precipitation (mm)")+
-  ylab("Residual fitted model")+
-  annotate("text", label = "R-squared = 0.03", x = 3200, y = -0.5) +
-  facet_wrap(~Type_Flux)
-
-# 1.7 Plot all graphs together
+# 1.4 Plot all graphs together
 pdf("Latex/Figures/Residual_Flux.eps", width = 17, height = 14) # Open a new pdf file
-grid.arrange(gg1, gg2, gg3, gg4, gg5, gg6, gg7, gg8, gg9, gg10, gg11, gg12, nrow=6) # Write the grid.arrange in the file
+grid.arrange(gg1, gg2, gg3, gg4, gg5, gg6, gg7, gg8, gg9, nrow=3) # Write the grid.arrange in the file
 dev.off() # Close the file
